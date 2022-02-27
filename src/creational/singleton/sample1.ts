f61ef5e641640200a8a8ba64d9f4608d4eababfd
Jovid Khamrokulov <jovid.kh@payme.uz>
2022-02-27 23:33:15 +0500

    Created sample for Singleton dp.

---
+++ src/creational/singleton/sample1.ts
@@ -0,0 +1,26 @@
+/**
+ *   Implementation of singleton design pattern in typescript.
+ *
+ *   Singleton is a creational design pattern that lets you
+ *   ensure that a class has only one instance while providing
+ *   a global access point to this instance
+ */
+
+class FileService {
+    private static _fileService: FileService = null;
+    private constructor() {}
+
+    public static getFileService() {
+        if (this._fileService === null) {
+            this._fileService = new FileService();
+        }
+        return this._fileService;
+    }
+
+    public readFileByUrl(url: string): void {
+        console.log('Opening ' + url + "...");
+    }
+}
+
+const fileService = FileService.getFileService();
+fileService.readFileByUrl('MyFile');
